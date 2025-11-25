be04a8a50a9763bd7b17d0f819c91f69c3c6e0d9
NgocHung11 <22654091.hung@student.iuh.edu.vn>
2025-11-25 15:00:42 +0700

    add genre routes: implement CRUD operations for genres with access control middleware

---
+++ backend/src/routes/genreRoute.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { protectedRoute, adminRoute } from "../middlewares/authMiddleware.js"
+import { getGenres, getGenreById, createGenre, updateGenre, deleteGenre } from "../controllers/genreController.js"
+
+const router = express.Router()
+
+// PUBLIC ROUTES
+router.get("/", getGenres)
+router.get("/:id", getGenreById)
+
+// ADMIN ROUTES
+router.post("/", protectedRoute, adminRoute, createGenre)
+router.put("/:id", protectedRoute, adminRoute, updateGenre)
+router.delete("/:id", protectedRoute, adminRoute, deleteGenre)
+
+export default router
